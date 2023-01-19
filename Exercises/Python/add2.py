fc0311ebcf41c8e9ab8ae0be658a8b655c497436
Birgitte Brydsö <bbrydsoe@gmail.com>
2023-01-19 15:28:27 +0100

    Create add2.py

---
+++ Exercises/Python/add2.py
@@ -0,0 +1,11 @@
+# This program will add two numbers that are provided by the user
+     
+# Get the numbers
+a = int(input("Enter the first number: ")) 
+b = int(input("Enter the second number: "))
+     
+# Add the two numbers together
+sum = a + b
+     
+# Output the sum
+print("The sum of {0} and {1} is {2}".format(a, b, sum))
