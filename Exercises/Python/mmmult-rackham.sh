8c2acaaf617e77ab4e82c56d147618994468e759
Birgitte Brydsö <bbrydsoe@gmail.com>
2023-02-07 20:40:22 +0100

    Create mmmult-rackham.sh

---
+++ Exercises/Python/mmmult-rackham.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+# Batch script for running mmmult.py on Rackham
+#SBATCH -A naiss2023-22-44 # Change to your own project ID after the course
+#SBATCH --time=00:10:00 # Asking for 10 minutes
+#SBATCH -n 1 # Asking for 1 core
+
+# Load any modules you need, here Python 3.9.5.
+module load python/3.9.5
+
+# Run your Python script
+python mmmult.py
