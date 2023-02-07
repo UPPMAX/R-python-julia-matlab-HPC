d2368fbcfba42361d27a839e78cd470ebee85636
Birgitte Brydsö <bbrydsoe@gmail.com>
2023-02-07 20:21:50 +0100

    Create sum-2args-rackham.sh

---
+++ Exercises/Python/sum-2args-rackham.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -A naiss2023-22-44 # Change to your own ID after the course
+#SBATCH --time=00:05:00 # Asking for 5 minutes
+#SBATCH -n 1 # Asking for 1 core
+
+# Load any modules you need, here for Python 3.9.5
+module load Python/3.9.5
+
+# Run your Python script
+python sum-2args.py 2 3
