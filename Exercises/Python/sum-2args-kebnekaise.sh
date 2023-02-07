866be77ce2a9471d86336d45916cdac670e25d89
Birgitte Brydsö <bbrydsoe@gmail.com>
2023-02-07 20:19:18 +0100

    Create sum-2args-kebnekaise.sh

---
+++ Exercises/Python/sum-2args-kebnekaise.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -A hpc2nXXXX-YYY # Change to your own ID
+#SBATCH --time=00:05:00 # Asking for 5 minutes
+#SBATCH -n 1 # Asking for 1 core
+
+# Load any modules you need, here for Python 3.9.5
+module load GCC/10.3.0  OpenMPI/4.1.1 Python/3.9.5
+
+# Run your Python script
+python sum-2args.py 2 3
