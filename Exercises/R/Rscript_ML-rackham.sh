d4af3d7b5f623d1b46c727236b1f7396cf4780ed
Birgitte Brydsö <bbrydsoe@gmail.com>
2023-02-09 17:31:08 +0100

    Create Rscript_ML-rackham.sh

---
+++ Exercises/R/Rscript_ML-rackham.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# A batch script for running the ML R program Rscript.R on Rackham
+#SBATCH -A naiss2023-22-44
+#Asking for 10 min.
+#SBATCH -t 00:10:00
+#SBATCH -n 1
+#Writing output and error files
+#SBATCH --output=output%J.out
+#SBATCH --error=error%J.error
+
+ml purge > /dev/null 2>&1
+ml R_packages/4.0.4
+
+R --no-save --no-restore -f Rscript.R
