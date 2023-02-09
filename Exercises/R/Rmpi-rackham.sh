bf00b701205e7c50b44a704b02ab7b13c78e3403
Birgitte Brydsö <bbrydsoe@gmail.com>
2023-02-09 17:29:08 +0100

    Create Rmpi-rackham.sh

---
+++ Exercises/R/Rmpi-rackham.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -A naiss2023-22-44
+#Asking for 10 min.
+#SBATCH -t 00:10:00
+#SBATCH -n 8
+
+export OMPI_MCA_mpi_warn_on_fork=0
+
+ml purge > /dev/null 2>&1
+ml R_packages/4.0.4
+ml openmpi/3.1.5
+
+mpirun -np 1 R CMD BATCH --no-save --no-restore Rmpi.R output.out
