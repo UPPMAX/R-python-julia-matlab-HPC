c387cc193133ae99931ef2ea5323f5e18aeebec6
Birgitte Brydsö <bbrydsoe@gmail.com>
2023-02-09 17:33:08 +0100

    Create Rscript_ML-kebnekaise.sh

---
+++ Exercises/R/Rscript_ML-kebnekaise.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# A batch script for running the ML R program Rscript.R on Kebnekaise 
+#SBATCH -A hpc2nXXXX-YYY # Change to your own project ID
+#Asking for 10 min.
+#SBATCH -t 00:10:00
+#SBATCH -n 1
+#Writing output and error files
+#SBATCH --output=output%J.out
+#SBATCH --error=error%J.error
+
+ml purge > /dev/null 2>&1
+ml GCC/10.2.0  OpenMPI/4.0.5
+ml R/4.0.4
+
+R --no-save --no-restore -f Rscript.R
