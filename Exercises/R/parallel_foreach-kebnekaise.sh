e45f4596c345c6d7d07942689207691f92229576
Birgitte Brydsö <bbrydsoe@gmail.com>
2023-02-09 17:28:04 +0100

    Create parallel_foreach-kebnekaise.sh

---
+++ Exercises/R/parallel_foreach-kebnekaise.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# A batch script for running the R program parallel_foreach.R on Kebnekaise 
+#SBATCH -A hpc2nXXXX-YYY # Change to your own project ID
+#SBATCH -t 00:10:00
+#SBATCH -N 1
+#SBATCH -c 4
+
+ml purge > /dev/null 2>&1
+ml GCC/10.2.0  OpenMPI/4.0.5  R/4.0.4
+
+# Batch script to submit the R program parallel_foreach.R
+R -q --slave -f parallel_foreach.R
