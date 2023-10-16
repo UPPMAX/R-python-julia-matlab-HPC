681dc873a751eebe37eee503882acc8d8b8eafbb
Birgitte Brydsö <bbrydsoe@cs.umu.se>
2023-10-16 16:46:36 +0200

    Additins to the GPU with Python session

---
+++ Exercises/Python/add-list-snowy.sh
@@ -0,0 +1,18 @@
+ #!/bin/bash
+# Remember to change this to your own project ID after the course!
+#SBATCH -A naiss2023-22-914
+# We are asking for 5 minutes
+#SBATCH --time=00:05:00
+# Asking for one GPU
+#SBATCH -M snowy            
+#SBATCH --gres=gpu:1   
+
+# Remove any loaded modules and load the ones we need
+module purge  > /dev/null 2>&1
+module load python/3.9.5  
+
+# Load virtual environment 
+source Example-gpu/bin/activate   
+	  
+# Run your Python script
+python add-list.py
