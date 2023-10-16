95d9993cc915028e5e3d877d8ed3def4b61c81de
Birgitte Brydsö <bbrydsoe@cs.umu.se>
2023-10-16 14:43:27 +0200

    More updates to exercises

--- Exercises/Python/mmmult-kebnekaise.sh
+++ Exercises/Python/mmmult-kebnekaise.sh
@@ -4,8 +4,8 @@
 #SBATCH --time=00:10:00 # Asking for 10 minutes
 #SBATCH -n 1 # Asking for 1 core
 
-# Load any modules you need, here Python 3.9.5 and prerequisites.
-module load GCC/10.3.0  OpenMPI/4.1.1 Python/3.9.5
+# Load any modules you need, here Python 3.9.5, SciPy-bundle,  and prerequisites.
+module load GCC/10.3.0  OpenMPI/4.1.1 Python/3.9.5 SciPy-bundle/2021.05
 
 # Run your Python script
 python mmmult.py
