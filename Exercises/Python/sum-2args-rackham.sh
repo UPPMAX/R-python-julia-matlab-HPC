970b310b2caecfacd32ac4679b840fd58f605e19
Birgitte Brydsö <bbrydsoe@gmail.com>
2023-10-16 20:03:16 +0200

    Update sum-2args-rackham.sh

--- Exercises/Python/sum-2args-rackham.sh
+++ Exercises/Python/sum-2args-rackham.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -A naiss2023-22-44 # Change to your own ID after the course
+#SBATCH -A naiss2023-22-914 # Change to your own ID after the course
 #SBATCH --time=00:05:00 # Asking for 5 minutes
 #SBATCH -n 1 # Asking for 1 core
 
