e0215c44234972a7290091244fa7b56f6b27e1df
Birgitte Brydsö <bbrydsoe@gmail.com>
2023-10-19 14:07:46 +0200

    Create add2-kebnekaise.sh

---
+++ Exercises/R/add2-kebnekaise.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# A batch script to run a serial R code on Kebnekaise
+#SBATCH -A hpc2n2023-110 # Course project id. Change to your own project ID after the course
+#SBATCH --time=00:10:00 # Asking for 10 minutes
+#SBATCH -n 1 # Asking for 1 core
+
+# Load any modules you need, here R/4.0.4
+module purge 
+module load GCC/10.2.0  OpenMPI/4.0.5  R/4.0.4
+
+# Run your R script (here 'add2.R') for some random numbers, here 234 and 346 
+Rscript --no-save --quiet add2.R 234 346
