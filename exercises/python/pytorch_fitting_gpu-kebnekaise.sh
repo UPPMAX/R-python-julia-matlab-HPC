c8fc418c722c18cf5d0d1e39f19fc578c26e90a5
Birgitte Brydsö <bbrydsoe@cs.umu.se>
2024-03-07 13:05:04 +0100

    Tweaking the GPU example batch script for Snowy

--- exercises/python/pytorch_fitting_gpu-kebnekaise.sh
+++ exercises/python/pytorch_fitting_gpu-kebnekaise.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Remember to change this to your own project ID after the course!
-#SBATCH -A hpc2n2023-110
+#SBATCH -A hpc2n2024-025
 # We are asking for 5 minutes
 #SBATCH --time=00:05:00
 # The following two lines splits the output in a file for any errors and a file for other output.
@@ -11,6 +11,6 @@
 
 # Remove any loaded modules and load the ones we need
 module purge  > /dev/null 2>&1
-module load GCC/10.3.0  OpenMPI/4.1.1 PyTorch/1.10.0-CUDA-11.3.1
+module load GCC/11.2.0 OpenMPI/4.1.1 PyTorch/1.12.1-CUDA-11.4.1
 
 srun python pytorch_fitting_gpu.py
--- exercises/python/pytorch_fitting_gpu-snowy.sh
+++ exercises/python/pytorch_fitting_gpu-snowy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Remember to change this to your own project ID after the course!
-#SBATCH -A naiss2023-22-914 
+#SBATCH -A naiss2024-22-107 
 # We are asking for 5 minutes
 #SBATCH --time=00:05:00
 # The following two lines splits the output in a file for any errors and a file for other output.
@@ -12,6 +12,7 @@
 # Asking for one GPU
 #SBATCH --gres=gpu:1
 
-module load python_ML_packages/3.9.5-gpu
+module load uppmax
+module load python_ML_packages/3.9.5-gpu python/3.9.5 
 
 srun python pytorch_fitting_gpu.py
