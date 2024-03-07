5c5e143aea3ad4baa0d76d76c73e35fc023aeae8
Birgitte Brydsö <bbrydsoe@cs.umu.se>
2024-03-07 14:50:38 +0100

    Trying to get a newer Python to work for the GPU numba example

--- exercises/python/add-list-kebnekaise.sh
+++ exercises/python/add-list-kebnekaise.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Batch script for running the numba program "add-list.py" on Kebnekaise
-#SBATCH -A hpc2n2023-110 # Remember to change this to your own project ID
+#SBATCH -A hpc2n2024-025 # Remember to change this to your own project ID
 # We are asking for 5 minutes
 #SBATCH --time=00:05:00
 # Asking for one V100
@@ -8,7 +8,7 @@
 
 # Remove any loaded modules and load the ones we need
 module purge  > /dev/null 2>&1
-module load GCC/10.3.0  OpenMPI/4.1.1 Python/3.9.5 SciPy-bundle/2021.05 CUDA/11.3.1
+module load GCC/12.3.0  OpenMPI/4.1.5 Python/3.11.3 SciPy-bundle/2023.07 CUDA/11.3.1
 
 # Run your Python script
 python add-list.py
