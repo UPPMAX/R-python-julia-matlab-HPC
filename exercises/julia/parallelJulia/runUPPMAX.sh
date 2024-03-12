6c3fe0a03b5cca7cd4fb725458f97c7f1158c200
Björn Claremar <70746791+bclaremar@users.noreply.github.com>
2024-03-12 16:03:52 +0100

    Update runUPPMAX.sh

--- exercises/julia/parallelJulia/runUPPMAX.sh
+++ exercises/julia/parallelJulia/runUPPMAX.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
-#SBATCH -A hpc2n2023-110     # your project_ID
+#!/bin/bash -l
+#SBATCH -A naiss2024-22-107     # your project_ID
 #SBATCH -J job-serial        # name of the job
-#SBATCH -n 1                 # nr. tasks
+##SBATCH -n 4                 # nr. tasks
+#SBATCH -c X                 # nr. of threads
 #SBATCH --time=00:20:00      # requested time
 #SBATCH --error=job.%J.err   # error file
 #SBATCH --output=job.%J.out  # output file
 
-ml purge  > /dev/null 2>&1
-ml Julia/1.8.5-linux-x86_64
+ml julia/1.8.5
 
 julia --threads X script-df.jl  # X number of threads
