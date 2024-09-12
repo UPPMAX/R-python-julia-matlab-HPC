080ecf64806253507e551d8ad40a50ebde3611e6
ColorOutOfSpace <20117570+rlpitts@users.noreply.github.com>
2024-09-12 16:25:09 +0200

    Create 2-parfor.m
    create matlab exercise folder and start populating it

---
+++ exercises/matlab/2-parfor.m
@@ -0,0 +1,12 @@
+function output = par_greeting(idx)
+planets = ["Mercury" "Venus" "Earth" "Mars" ...
+    "Jupiter" "Saturn" "Uranus" "Neptune"]
+tic
+% add (parallel) for-loop that prints "Hello from"+planets(idx) below:
+
+
+
+toc
+
+
+
