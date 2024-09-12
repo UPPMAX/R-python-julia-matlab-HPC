cef33393a2af4d032f76a6cbe4b30d766e359e79
ColorOutOfSpace <20117570+rlpitts@users.noreply.github.com>
2024-09-12 17:07:42 +0200

    Update 2-parfor.m
    made the code more fun

--- exercises/matlab/2-parfor.m
+++ exercises/matlab/2-parfor.m
@@ -1,12 +1,16 @@
-function output = par_greeting(idx)
-planets = ["Mercury" "Venus" "Earth" "Mars" ...
-    "Jupiter" "Saturn" "Uranus" "Neptune"]
+function t_out = par_greeting(n_planets)
+planets = ["Mercury" "Venus" "Earth" "Mars" "Ceres" ...
+    "Jupiter" "Saturn" "Uranus" "Neptune" "Pluto" ...
+    "Orcus" "Salacia" "Haumea" "Quaoar" "Makemake" ...
+    "Gonggong" "Eris" "Sedna"];
+if n_planets < 1 || n_planets > length(planets)
+    error("Please choose a number from 1 to 18.")
 tic
 % add (parallel) for-loop that prints "Hello from"+planets(idx) below:
 
 
 
-toc
-
+t_out = toc;
+end
 
 
