4685c666ccd8b1d9b8d29d1c7edb7f356e0a8448
Pedro Ojeda-May <pedroojeda2011@gmail.com>
2024-10-18 09:50:34 +0200

    Logo including lunarc

--- docs/conf.py
+++ docs/conf.py
@@ -99,7 +99,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_logo = "../img/logo-python-hpc-course.svg"
+html_logo = "../img/hpc2n-lunarc-uppmax-hpc-course.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
