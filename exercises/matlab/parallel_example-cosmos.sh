56a359ec9d8699930d009e570649f716271b2795
Birgitte Brydsö <bbrydsoe@cs.umu.se>
2024-10-20 19:25:44 +0200

    Adding some batch scripts for the parallel example

---
+++ exercises/matlab/parallel_example-cosmos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Change to your actual project number
+#SBATCH -A lu2024-7-80
+# Remember, there are 4 workers and 1 master!
+#SBATCH --ntasks=5
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=5
+#SBATCH --ntasks-per-core=1
+# Asking for 30 min (change as you want)
+#SBATCH -t 00:30:00
+#SBATCH --error=matlab_%J.err
+#SBATCH --output=matlab_%J.out
+
+# Clean the environment
+module purge > /dev/null 2>&1
+
+# Change depending on resource and MATLAB version
+# to find out available versions: module spider matlab
+module add matlab/2023b
+
+# Executing a parallel matlab program
+srun matlab -nojvm -nodisplay -nodesktop -nosplash -r "parallel_example(16)"
