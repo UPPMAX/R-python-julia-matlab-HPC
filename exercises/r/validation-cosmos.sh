ef0a76218fb825c3e95a3bf8d98f23a37bcf18d0
Birgitte Brydsö <bbrydsoe@gmail.com>
2024-10-24 16:02:50 +0200

    Update validation-cosmos.sh

--- exercises/r/validation-cosmos.sh
+++ exercises/r/validation-cosmos.sh
@@ -9,5 +9,8 @@ module load GCC/11.3.0  OpenMPI/4.1.4  R/4.2.1 R-bundle-Bioconductor/3.15-R-4.2.
 #Make sure we use the renv with datarium installed - this was installed under the "Isolated environments with renv" section 
 cd <path-to-my-renv-project/<my-renv>
 
+#Another way to use the renv would be to load it - in that case you don't need to have your R script in the same directory as the renv. Comment out the above and uncomment the below if you want to do this.  
+#renv::load("<path-to-your-renv>")
+
 # Run your R script (here 'validation.R'. Give the path if it is not located in the same directory as the renv)
 R --no-save --quiet < <path-to-script>/validation.R
